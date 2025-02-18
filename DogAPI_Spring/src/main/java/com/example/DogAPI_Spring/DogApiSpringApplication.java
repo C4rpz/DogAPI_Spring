c9f3379141e1ec607c7b9a6a9b394797a6d55ae6
C4rpz <153099491+C4rpz@users.noreply.github.com>
2025-02-18 16:36:12 +0100

    Add files via upload

---
+++ DogAPI_Spring/src/main/java/com/example/DogAPI_Spring/DogApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.DogAPI_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogApiSpringApplication.class, args);
+	}
+
+}
---
+++ DogAPI_Spring/src/test/java/com/example/DogAPI_Spring/DogApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DogAPI_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
